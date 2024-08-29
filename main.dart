574a95e6f232b4a8f69d665c739a55bc13dc5f8a
Paul Sombrador <jpsombrador@gmail.com>
2024-08-29 13:37:12 +0800

    Ilove GRC

---
+++ main.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+
+void main () => runApp (MyApp());
+
+class MyApp extends StatefulWidget {
+  @override
+  MyAppState createState() => MyAppState ();
+}
+class MyAppState extends State<MyApp> {
+  double _numberForm=0;
+
+  final List<String> _measures = [
+    'meters',
+    'kilometers',
+    'grams',
+    'kilogram',
+    'feet',
+    'miles',
+    'pounds(lbs)',
+    'Bounds',
+  ];
+
+  void initState(){
+    _numberForm = 0;
+    super.initState();
+  }
+
+  @override
+  Widget build (BuildContext context){
+
+    return MaterialApp(
+      title: 'Measures Converter',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Measures Converter'),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+
+              DropdownButton(
+                items: _measures.map ((String value){
+                  return DropdownMenuItem<String>(
+                    value: value,
+                    child: Text(value),
+                  );
+                }).toList(),
+                onChanged: (_){},
+              ),
+              TextField(
+                onChanged: (text){
+                  var rv=double.tryParse(text);
+                  if(rv !=null){
+                    setState(() {
+                      _numberForm =rv;
+                    });
+                  }
+                },
+              ),
+              Text(
+                (_numberForm ==null ? '': _numberForm.toString()),
+              )
+
+            ],
+          ),
+
+        ),
+      ),
+    );
+  }
+}
+
+
